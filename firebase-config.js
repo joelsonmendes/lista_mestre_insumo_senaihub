28a114c17f4a1c944606fb4f9be96dc44642e21f
joelsonmendes <joelson.mendes@senaiac.org.br>
2026-08-05 17:10:35 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Cole aqui as variáveis do seu aplicativo Web criado no Firebase Console.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDu4u3yH21bzvfXSUN4nycPw4qlr6ngqWM",
+  authDomain: "lista-mestre-278a2.firebaseapp.com",
+  projectId: "lista-mestre-278a2",
+  storageBucket: "lista-mestre-278a2.firebasestorage.app",
+  messagingSenderId: "576774638682",
+  appId: "1:576774638682:web:78d4df912f7b3ee89edc63"
 };
